5bf7028019d2df633db27f618c790100b2fa228d
lookto <git@lookto.de>
2022-05-06 18:03:36 +0200

    add cors middleware

--- server/api/src/index.js
+++ server/api/src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+var cors = require("cors");
 
 const etfRouter = require("./routes/etf");
 const etfProviderRouter = require("./routes/etfProvider");
@@ -11,6 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 app.use("/etf", etfRouter);
 app.use("/provider", etfProviderRouter);
 app.use("/index", etfIndexRouter);
