36b6a20545be587c60ee98ebdbba1a420325fbf6
lookto <git@lookto.de>
2022-05-07 15:22:03 +0200

    initialize react app

---
+++ client/web-app/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import "./App.css";
+
+export default function App() {
+    return (
+        <Router>
+        </Router>
+    );
+}
