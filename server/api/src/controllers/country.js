786473b555ef61657fa8d334ca0d5591c3c26dfa
lookto <git@lookto.de>
2022-06-14 18:35:13 +0200

    extend country model

--- server/api/src/controllers/country.js
+++ server/api/src/controllers/country.js
@@ -2,11 +2,12 @@ const db = require("../models");
 
 const getAllCountries = async (req, res) => {
     try {
-        console.log("Catching countries");
         const countries = await db["country"].findAll();
         const mappedCountries = countries.map((country) => ({
             id: country.dataValues.id,
             name: country.dataValues.name,
+            region: country.dataValues.region,
+            market: country.dataValues.market || null,
         }));
         res.send(mappedCountries);
     } catch (error) {
--- server/api/src/models/country.js
+++ server/api/src/models/country.js
@@ -19,6 +19,10 @@ module.exports = (sequelize, DataTypes) => {
                 type: DataTypes.STRING,
                 allowNull: false,
             },
+            market: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
         },
         {
             freezeTableName: true,
